199478fffe93d368e117867adc75f19993303606
Carlo Alberto Ferraris <carlo.ferraris@rakuten.com>
2015-12-18 22:22:33 +0900

    Documentation fixes

--- goropool.go
+++ goropool.go
@@ -7,23 +7,25 @@ import (
 	"sync"
 )
 
-// NewPool creates a bounded goroutine pool with workers goroutine accepting
-// work on a queue with queueSize elements.
+// NewPool creates and starts a bounded goroutine pool with numWorkers goroutine
+// accepting work on a queue with queueSize elements. Each pool is made up of
+// numWorkers+1 goroutines. NewPool returns immediately.
+//
 // Two channels are returned: queue and done. The former is the channel jobs
 // should be submitted on. A job is simply a func(). Closing the queue channel
 // signals to the pool that no more jobs will be enqueued. Once the queue
 // channel has been closed and all queued jobs have been completed the pool will
 // close the second channel (done) to signal that the pool has shut down.
-// Each pool is made up of workers+1 goroutines. NewPool returns immediately.
+//
 // The done channel is of type error for future extensibility, currently no
 // error will be returned under any circumstances. The pool makes no attempt to
 // recover panicking jobs.
-func NewPool(workers, queueSize int) (chan<- func(), <-chan error) {
+func NewPool(numWorkers, queueSize int) (chan<- func(), <-chan error) {
 	queue := make(chan func(), queueSize)
 	done := make(chan error)
 	var wg sync.WaitGroup
-	wg.Add(workers)
-	for i := 0; i < workers; i++ {
+	wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ {
 		go func() {
 			defer wg.Done()
 			for job := range queue {
